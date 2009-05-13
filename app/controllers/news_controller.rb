3159f086c8976d385fe23ec0908c6caa30281f7f
Ratan Sebastian <rjsvaljean@gmail.com>
2009-05-13 11:47:19 +0530

    fixed bug with unset languages

--- app/controllers/news_controller.rb
+++ app/controllers/news_controller.rb
@@ -1,6 +1,7 @@
 class NewsController < ApplicationController
   radiant_layout 'Base'
   no_login_required
+  beffore_filter :set_language
 
   def index
     begin
@@ -20,4 +21,29 @@ class NewsController < ApplicationController
     end
   end
 
+  private
+  def accepted_languages()
+    # no language accepted
+    return [] if request.env["HTTP_ACCEPT_LANGUAGE"].nil?
+
+    # parse Accept-Language
+    accepted = request.env["HTTP_ACCEPT_LANGUAGE"].split(",")
+    accepted = accepted.map { |l| l.strip.split(";") }
+    accepted = accepted.map { |l|
+      if (l.size == 2)
+        # quality present
+        [ l[0].split("-")[0].downcase, l[1].sub(/^q=/, "").to_f ]
+      else
+        # no quality specified =&gt; quality == 1
+        [ l[0].split("-")[0].downcase, 1.0 ]
+      end
+    }
+
+    # sort by quality
+    accepted.sort { |l1, l2| l1[1] <=> l2[1] }
+  end
+
+  def set_language
+    session[:language] ||= accept_language.first[0..1]
+  end
 end
