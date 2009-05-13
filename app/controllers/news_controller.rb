120c5f5553bf1f73d0dac9f67f35b0df935708a7
Ratan Sebastian <rjsvaljean@gmail.com>
2009-05-13 12:29:10 +0530

    minor edit

--- app/controllers/news_controller.rb
+++ app/controllers/news_controller.rb
@@ -22,28 +22,27 @@ class NewsController < ApplicationController
   end
 
   private
-  def accepted_languages()
-    # no language accepted
-    return [] if request.env["HTTP_ACCEPT_LANGUAGE"].nil?
-
-    # parse Accept-Language
-    accepted = request.env["HTTP_ACCEPT_LANGUAGE"].split(",")
-    accepted = accepted.map { |l| l.strip.split(";") }
-    accepted = accepted.map { |l|
-      if (l.size == 2)
-        # quality present
-        [ l[0].split("-")[0].downcase, l[1].sub(/^q=/, "").to_f ]
-      else
-        # no quality specified =&gt; quality == 1
-        [ l[0].split("-")[0].downcase, 1.0 ]
-      end
-    }
-
-    # sort by quality
-    accepted.sort { |l1, l2| l1[1] <=> l2[1] }
-  end
-
+#  def accepted_languages()
+#    # no language accepted
+#    return [] if request.env["HTTP_ACCEPT_LANGUAGE"].nil?
+#
+#    # parse Accept-Language
+#    accepted = request.env["HTTP_ACCEPT_LANGUAGE"].split(",")
+#    accepted = accepted.map { |l| l.strip.split(";") }
+#    accepted = accepted.map { |l|
+#      if (l.size == 2)
+#        # quality present
+#        [ l[0].split("-")[0].downcase, l[1].sub(/^q=/, "").to_f ]
+#      else
+#        # no quality specified =&gt; quality == 1
+#        [ l[0].split("-")[0].downcase, 1.0 ]
+#      end
+#    }
+#
+#    # sort by quality
+#    accepted.sort { |l1, l2| l1[1] <=> l2[1] }
+#  end
   def set_language
-    session[:language] ||= accepted_languages.first[0..1]
+    session[:language] ||= 'en'
   end
 end
